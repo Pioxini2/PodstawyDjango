5d46f30dba87beb28bf4b8c205a736415bb7b54e
Pioxini2 <piootrek2005@wp.pl>
2023-11-27 09:36:07 -0500

    Wyswietlanie userow z bazy; Fun. get_users

--- mysite/calculator/urls.py
+++ mysite/calculator/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import calc, hello
+from .views import calc, get_users, hello
 
 urlpatterns = [
     path('hello/<int:number>', hello),
-    path('calc', calc)
+    path('calc', calc),
+    path('users', get_users)
 ]
--- mysite/calculator/views.py
+++ mysite/calculator/views.py
@@ -2,8 +2,10 @@ import json
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+
+from calculator.models import User
 
 def hello(request, number):
     print(request)
@@ -17,6 +19,17 @@ def calc(request):
     if data["operation"] == "+":
         result = data["a"]+data["b"]
     return HttpResponse(f"{result}")
+
+def get_users(request):
+    users = User.objects.all()
+
+    #return HttpResponse(f"{users}")
+
+    users_data=[]
+    for user in users:
+        users_data.append(user.username)
+
+    return JsonResponse({"users" : users_data})
 
 '''def factorial(request):
     return HttpResponse("Tu bedzie silnia")'''
